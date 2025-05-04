1cafdc85e973d3a4dbaaf993d8127da96fbd9afb
GaneshPrasad14 <128919436+GaneshPrasad14@users.noreply.github.com>
2025-05-05 00:26:04 +0530

    Update imports.js

--- src/imports/imports.js
+++ src/imports/imports.js
@@ -4,3 +4,6 @@ export {default as HexaEditProfile} from '../features/HexaEditProfile';
 export {default as HexaWelcomeScreen} from '../features/HexaWelcomeScreen';
 export {default as HexaLoginScreen} from '../features/HexaLoginScreen';
 export {default as HexaSignUpScreen} from '../features/HexaSignUpScreen';
+export {default as ForgotPassword } from '../screens/ForgotPassword';
+export {default as OTPVerification } from '../screens/OTPVerification';
+export {default as ResetPassword } from '../screens/ResetPassword';
